aabf866a4c5a8520b9323b66ae3a217aea5515a6
Anthony Robin <amrobin@oakland.edu>
2019-02-10 00:11:36 -0500

    BeerSong Ninety_Nine_Bottles_of_Beer method add

--- interface/BeerSong.java
+++ interface/BeerSong.java
@@ -1,9 +1,24 @@
-main () {
+public class BeerSong {
+
+public static void main(String[] args) {
+
+
 
         }
 
+public static void Ninety_Nine_Bottles_of_Beer() {
+    int i = 99;
+    int j = 98;
 
-Ninety_Nine_Bottles_of_Beer() {
 
+        while(i !=0 && j !=0) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer");
+            System.out.println("take one down, pass it around, " + j + " bottles of beer on the wall");
+            i--;
+            j--;
+        }
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer ");
+        System.out.println("take one down, pass it around, no more bottles of beer on the wall.");
 
         }
+}
